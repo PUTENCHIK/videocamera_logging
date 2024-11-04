b2e9674aee788dec61e99fa4f8714d0f878ab691
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-11-04 20:47:30 +0800

    Added rendering of temlates from static/

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static/css", StaticFiles(directory="static/css"), name="styles")
+
+templates = Jinja2Templates(directory="static/html")
 
 
-@app.get("/")
-async def root():
-    return {'message': "Hello on Videocamera Logging!"}
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
 
 
 @app.get("/{any}")

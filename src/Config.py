c8df9d0be0e14e56743b63a8e3c398d89edb1268
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2025-03-19 22:02:14 +0800

    Added class wraper for YOLO model class, a few trained models into storage/models

--- src/Config.py
+++ src/Config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from fastapi.templating import Jinja2Templates
 
 
@@ -7,3 +8,20 @@ class Config:
     app_port = 5050
 
     templates = Jinja2Templates(directory="static/html")
+
+    model_storage = Path("storage/models")
+    model_name = "yolo11l_100.pt"
+    model_confidence = 0.75
+    detecting_classes_names = {
+        0: 'human',
+        1: 'bear',
+        2: 'elk',
+        3: 'fox',
+    }
+    # BGR encoding
+    detecting_classes_colors = {
+        0: (53, 124, 240),
+        1: (52, 235, 161),
+        2: (252, 50, 192),
+        3: (235, 212, 38),
+    }
